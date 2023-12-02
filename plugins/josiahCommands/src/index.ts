42fcf90e07a94a4dda6f43017e3e315f53987a8d
Josiah <65928867+josaih-mo@users.noreply.github.com>
2023-12-02 13:22:57 +0900

    keep modifying the code until it works

--- plugins/josiahCommands/src/index.ts
+++ plugins/josiahCommands/src/index.ts
@@ -1,6 +1,8 @@
 import { logger, commands } from "@vendetta";
 import Settings from "./Settings";
 import { msgInput, sendMessage } from "./josiahLib-vendetta/josiahLib-vendetta"
+import { findByProps } from "@vendetta/metro";
+let lce = findByProps("Messages")
 
 //#region Commands
 
@@ -23,22 +25,27 @@ let patches = [];
 
 export default {
     onLoad: () => {
-        logger.log("Loading commands!");
         patches.push(commands.registerCommand({
             name: "happy",
             description: "a very happy boi :D",
             //@ts-ignore
             applicationId: -1,
-            type: ApplicationCommandType.MESSAGE,
-            inputType: ApplicationCommandInputType.BOT,
-            options: [msgInput],
+            type: 1,
+            inputType: 1,
+            options: [{
+                name: "message",
+                description: lce.Messages.COMMAND_SHRUG_MESSAGE_DESCRIPTION,
+                required: true,
+                displayDescription: lce.Messages.COMMAND_SHRUG_MESSAGE_DESCRIPTION,
+                displayName: "message",
+                type: 3
+            }],
             execute: (args, ctx) => {
                 sendMessage(ctx.channel.id, args[0].value + "\\(^v^)/")
             }
         }))
     },
     onUnload: () => {
-        logger.log("Goodbye discord!!!");
         for (const unpatch of patches) unpatch();
     },
     settings: Settings,
